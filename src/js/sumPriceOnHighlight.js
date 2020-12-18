d36b488665a2df76dab6865d7e8e7f09d835fe2d
DEVRafam <jodax95@gmail.com>
2020-12-18 05:08:32 +0100

    public dir fixes && js code refactor

--- src/js/sumPriceOnHighlight.js
+++ src/js/sumPriceOnHighlight.js
@@ -1,12 +1,10 @@
-export default (className) => {
-    let sum = 0;
-    //
-    [...document.querySelectorAll(`.${className}`)].forEach((pricePart) => {
-        sum += pricePart.textContent.split(" ")[0] * 1;
-    });
-    //
-    const sumParts = String(sum).split(".");
-    sum = `${sumParts[0]}.${sumParts[1].slice(0, 2)}`;
-    //
-    document.getElementById("price-sum").textContent = `${sum} zł`;
-};
+let sum = 0;
+//
+[...document.querySelectorAll(`.${"price-part"}`)].forEach((pricePart) => {
+    sum += pricePart.textContent.split(" ")[0] * 1;
+});
+//
+const sumParts = String(sum).split(".");
+sum = `${sumParts[0]}.${sumParts[1].slice(0, 2)}`;
+//
+document.getElementById("price-sum").textContent = `${sum} zł`;
--- src/js/zipCodeInput.js
+++ src/js/zipCodeInput.js
@@ -1,9 +1,7 @@
-export default () => {
-    const zip = document.getElementById("kod");
-    zip.addEventListener("keydown", () => {
-        let v = String(zip.value);
-        if (v.length > 2 && v.indexOf("-") == -1) {
-            zip.value = `${v.slice(0, 2)}-${v.slice(2)}`;
-        }
-    });
-};
+const zip = document.getElementById("kod");
+zip.addEventListener("keydown", () => {
+    let v = String(zip.value);
+    if (v.length > 2 && v.indexOf("-") == -1) {
+        zip.value = `${v.slice(0, 2)}-${v.slice(2)}`;
+    }
+});
